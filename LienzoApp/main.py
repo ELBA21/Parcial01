c00e9534c27e50811af729465f6a16e1baebdf70
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2023-11-20 15:23:25 -0300

    me rindo

---
+++ LienzoApp/main.py
@@ -0,0 +1,11 @@
+from Acesorio import Acesorio
+from Lapiz import Lapiz
+from Pincel import Pincel
+from Spray  import Spray
+if __name__ == "__main__":
+    A = Acesorio(0,"","",0,0)
+    L = Lapiz(1, "Lapiz", "Rojo", 3,1)
+    P = Pincel(2, "Pincel", "Rojo", 3, 1)
+    S = Spray(3,"Spray", "Rojo", 3,1)
+
+    print("Seleccione un Accesorio para dibujar")
